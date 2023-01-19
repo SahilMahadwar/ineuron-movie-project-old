14ec3a7b1a12195d2b4b52aa05393bc291aad30c
SahilMahadwar <accounts@mahadwar.com>
2023-01-19 22:28:35 +0530

    🎉 Project setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
